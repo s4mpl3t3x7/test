d88152dcb058bd994320675f7329272d75adad2e
s4mpl3t3x7 <s4mpl3t3x7@gmail.com>
2018-12-27 00:26:10 +0300

    Добавление строки

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -11,6 +11,7 @@ namespace GitHubTest
         static void Main(string[] args)
         {
             Console.WriteLine("Первое знакомство с GitHub");
+            Console.WriteLine("Проверка изменений на сервере");
         }
     }
 }
