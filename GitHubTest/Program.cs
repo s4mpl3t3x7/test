5394f61537be6af201a210243cadaaed7f77d5c2
s4mpl3t3x7 <s4mpl3t3x7@gmail.com>
2018-12-27 00:31:16 +0300

    45754

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -10,7 +10,7 @@ namespace GitHubTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello GitHub");
+            Console.WriteLine("OK GitHub");
             Console.WriteLine("Проверка изменений на сервере");
         }
     }
